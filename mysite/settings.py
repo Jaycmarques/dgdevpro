b6b9eab48dc1cf2d78f7e1c22d5bcbee5cbc6310
Julio Marques <julio.jcmarques@gmail.com>
2024-05-13 16:48:31 -0300

    CSS e HTML realizado

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--f!if-)h*%5p(rylmt0$6f(6i%)pp)j3ms9o2^_4anoye^if1+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'jcmarques.pythonanywhere.com']
 
 
 # Application definition
