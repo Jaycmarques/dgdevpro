2983174467d3b1f81125d24602a559649f0a4bd3
Julio Marques <julio.jcmarques@gmail.com>
2024-06-03 23:51:53 -0300

    ajuste flake8

--- blog/test_mail_sender.py
+++ blog/test_mail_sender.py
@@ -8,7 +8,6 @@ def test_send_mail():
 @pytest.mark.parametrize('receiver', ['julio.jcmarques@gmail.com', 'leticiapie@icloud.com'])
 def test_receiver(receiver):
     sender = Sender()
-    receivers = ['julio.jcmarques@gmail.com', 'leticiapie@icloud.com']
     result = sender.send(receiver, 'julionihongo@gmail.com', 'criação enviar email', 'aprendendo a criar testes de spam')
 
     assert receiver in result
