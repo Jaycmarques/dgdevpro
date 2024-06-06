4a6838285d6c40ab12359317c6222c81da6b7b92
Julio Marques <julio.jcmarques@gmail.com>
2024-06-05 23:28:03 -0300

    importando Mock fro unittest

--- blog/tests/test_envio_para_base_usuarios.py
+++ blog/tests/test_envio_para_base_usuarios.py
@@ -1,3 +1,4 @@
+from unittest.mock import Mock
 from blog.spam.mail_sender import Enviador
 from blog.spam.main import EnviadorDeSpam
 from blog.models import User
@@ -19,32 +20,21 @@ import pytest
 def test_qde_de_spam(sessao, users):
     for user in users:
         sessao.salvar(user)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('julio@gmail.com', 'Assunto', 'corpo')
-    assert len(users) == enviador.qde_email_enviados
+    assert len(users) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     user = User(nome='Julio', email='julio@gmail.com')
     sessao.salvar(user)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('lucas@gmail.com', 'Assunto', 'Corpo')
-    assert enviador.parametros_de_envios == (
+    enviador.enviar.assert_called_once_with(
         'lucas@gmail.com',
         'julio@gmail.com',
         'Assunto',
         'Corpo'
     )
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qde_email_enviados = 0
-        self.parametros_de_envios = None
-
-    def enviar(self, remetente, enviador, assunto, corpo):
-        self.parametros_de_envios = (remetente, enviador, assunto, corpo)
-        self.qde_email_enviados += 1
