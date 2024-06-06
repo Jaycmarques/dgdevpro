0674f8be54e087fdc19a2704065c52a38fec18d2
Julio Marques <julio.jcmarques@gmail.com>
2024-06-05 23:21:05 -0300

    criação Mock class

--- blog/spam/mail_sender.py
+++ blog/spam/mail_sender.py
@@ -1,11 +1,7 @@
 class Enviador:
-    def __init__(self):
-        self.qde_email_enviados = 0
-
     def enviar(self, remetente, enviador, assunto, corpo):
         if '@' not in remetente:
             raise EmailNotValid(f'Receiver e-mail not valid: {remetente}')
-        self.qde_email_enviados += 1
         return remetente
 
 
--- blog/tests/test_envio_para_base_usuarios.py
+++ blog/tests/test_envio_para_base_usuarios.py
@@ -19,7 +19,32 @@ import pytest
 def test_qde_de_spam(sessao, users):
     for user in users:
         sessao.salvar(user)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('julio@gmail.com', 'Assunto', 'corpo')
     assert len(users) == enviador.qde_email_enviados
+
+
+def test_parametros_de_spam(sessao):
+    user = User(nome='Julio', email='julio@gmail.com')
+    sessao.salvar(user)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails('lucas@gmail.com', 'Assunto', 'Corpo')
+    assert enviador.parametros_de_envios == (
+        'lucas@gmail.com',
+        'julio@gmail.com',
+        'Assunto',
+        'Corpo'
+    )
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qde_email_enviados = 0
+        self.parametros_de_envios = None
+
+    def enviar(self, remetente, enviador, assunto, corpo):
+        self.parametros_de_envios = (remetente, enviador, assunto, corpo)
+        self.qde_email_enviados += 1
